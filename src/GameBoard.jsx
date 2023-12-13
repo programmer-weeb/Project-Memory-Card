2f1ca6e3e87a982276bbf128ce9ba5534d8080a8
programmer-weeb <forprogramming8@gmail.com>
2023-12-13 19:18:22 +0200

    Add a lil style to make cats cards

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable linebreak-style */
 import React, { useState } from 'react'
 
@@ -27,23 +28,44 @@ export default function GameBoard({
 	return (
 		<div>
 			<h1>cards</h1>
-			{cardsArray.map((el) => {
-				return (
+			<div style={cardContainerStyle}>
+				{cardsArray.map((el) => (
 					<div
 						key={el.id}
-						style={{
-							display: 'flex',
-							alignContent: 'center',
-							alignItems: 'center',
-							direction: '',
-						}}
+						style={elementOfCardArrayStyle}
 						onClick={(event) => handleClickingACard(event, el)}
 					>
-						<img src={el.images.fixed_height.url} alt="" />
-						<p>{el.slug}</p>
+						<img
+							src={el.images.fixed_height.url}
+							alt=""
+							style={{ width: '100%', height: 'auto' }}
+						/>
+						<p
+							style={{
+								padding: '12px',
+								textAlign: 'center',
+								fontSize: '16px',
+							}}
+						>
+							{el.title}
+						</p>
 					</div>
-				)
-			})}
+				))}
+			</div>
 		</div>
 	)
+}
+
+const cardContainerStyle = {
+	display: 'grid',
+	gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+	gap: '16px',
+	padding: '16px',
+}
+
+const elementOfCardArrayStyle = {
+	border: '1px solid #ccc',
+	borderRadius: '8px',
+	overflow: 'hidden',
+	boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
 }
