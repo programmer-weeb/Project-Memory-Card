1219eb42e738d19f4509870d5d10626ce6abde5f
programmer-weeb <forprogramming8@gmail.com>
2023-12-13 10:35:58 +0200

    Add initial setup (empty)

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+import React, { useState } from 'react'
+import Header from './Header'
+import GameBoard from './GameBoard'
+import './App.css'
+
+function App() {
+	return (
+		<>
+			<Header />
+			<GameBoard />
+		</>
+	)
+}
+
+export default App
