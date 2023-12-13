ebfde7e8459faa936d9ca4d9ce8448a91cdac052
programmer-weeb <forprogramming8@gmail.com>
2023-12-13 18:19:46 +0200

    Add clicked items' id to an array

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -1,7 +1,26 @@
 /* eslint-disable linebreak-style */
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function GameBoard({ cardsArray, shuffleCardsArray }) {
+	const [idOfClickedItems, setClickedItems] = useState([])
+
+	function handleClickingACard(event, anElementOfArray) {
+		console.log({ event, anElementOfArray })
+
+		shuffleCardsArray()
+		setClickedItems((prevClickedItems) => {
+			// Check if the element already exists in the array
+			if (!prevClickedItems.includes(anElementOfArray.id)) {
+				// If it doesn't exist, add it to the array
+				return [...prevClickedItems, anElementOfArray.id]
+			} else {
+				// If it exists, return the current array without any changes
+				return prevClickedItems
+			}
+		})
+		console.log(idOfClickedItems)
+	}
+
 	return (
 		<div>
 			<h1>cards</h1>
@@ -15,9 +34,10 @@ export default function GameBoard({ cardsArray, shuffleCardsArray }) {
 							alignItems: 'center',
 							direction: '',
 						}}
-						
+						onClick={(event) => handleClickingACard(event, el)}
 					>
-						<img src={el.images.fixed_height.url} alt="" onClick={shuffleCardsArray} />
+						{/* <img src={el.images.fixed_height.url} alt="" onClick={shuffleCardsArray} /> */}
+						<img src={el.images.fixed_height.url} alt="" />
 						{/* {JSON.stringify(el)} */}
 						{/* {JSON.stringify(el.images.fixed_height.url)} */}
 						{/* <br /><br /> */}
